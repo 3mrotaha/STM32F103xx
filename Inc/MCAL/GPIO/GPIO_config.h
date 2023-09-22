e4fc2c7ae6970c63b1162411f7a6fbba0425971e
Amr M. Taha <3mrotaha@github.com>
2023-09-22 21:50:53 +0300

    edited

--- Inc/MCAL/GPIO/GPIO_config.h
+++ Inc/MCAL/GPIO/GPIO_config.h
@@ -9,7 +9,7 @@
 */
 #define GPIOA_ENABLE    ENABLE_PORT
 #define GPIOB_ENABLE    DISABLE_PORT
-#define GPIOC_ENABLE    DISABLE_PORT
+#define GPIOC_ENABLE    ENABLE_PORT
 #define GPIOD_ENABLE    DISABLE_PORT
 #define GPIOE_ENABLE    DISABLE_PORT
 /***************************** PORT A *****************************************/
